82cccebb99249ae843e38877e11864999e37c936
fit2-zhao <yong.zhao@fit2cloud.com>
2024-11-20 16:09:49 +0800

    test: 测试 action

--- backend/src/main/java/io/demo/crm/Application.java
+++ backend/src/main/java/io/demo/crm/Application.java
@@ -11,22 +11,21 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication(exclude = {
-        QuartzAutoConfiguration.class,  // 禁用 Quartz 自动配置
-        LdapAutoConfiguration.class,    // 禁用 LDAP 自动配置
-        Neo4jAutoConfiguration.class    // 禁用 Neo4j 自动配置
+        QuartzAutoConfiguration.class,
+        LdapAutoConfiguration.class,
+        Neo4jAutoConfiguration.class
 })
 @PropertySource(value = {
-        "classpath:commons.properties",   // 加载类路径下的配置文件
-        "file:/opt/demo/conf/demo.properties",  // 加载外部配置文件
-}, encoding = "UTF-8", ignoreResourceNotFound = true)  // 忽略配置文件未找到的错误
-@ServletComponentScan // 启用 Servlet 组件扫描，支持过滤器和监听器
+        "classpath:commons.properties",
+        "file:/opt/demo/conf/demo.properties",
+}, encoding = "UTF-8", ignoreResourceNotFound = true)
+@ServletComponentScan
 @EnableConfigurationProperties({
-        MinioProperties.class,  // 启用自定义配置类的绑定
+        MinioProperties.class,
 })
 public class Application {
 
     public static void main(String[] args) {
-        // 启动 Spring Boot 应用
         SpringApplication.run(Application.class, args);
     }
 }
